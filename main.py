12ef3bf0a9d5ea205a64dad9bf8b6fe5f7f0065a
BartlomiejOlber <300237@pw.edu.pl>
2021-03-16 20:20:45 +0100

    clean data

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-# This is a sample Python script.
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+MAX_PRICE = 100000
 
+
+def clean_products():
+    products = pd.read_json("data/products.jsonl", lines=True)
+    products.loc[products["price"] < 0, "price"] *= -1
+    products = products[products["price"] < MAX_PRICE]
+    products["category_path"] = products["category_path"].str.rsplit(';', n=1, expand=True)[1]
+    products.rename(columns={"category_path": "category"}, inplace=True)
+    products.to_json("data/products_clean.jsonl", orient='records', lines=True)
+    return products
+
+
+def clean_sessions():
+    sessions = pd.read_json("data/sessions.jsonl", lines=True)
+    products = pd.read_json("data/products_clean.jsonl", lines=True)
+    print(sessions.shape[0])
+    sessions = sessions[sessions["product_id"].isin(products["product_id"])]
+    sessions = sessions.fillna({"user_id": sessions.groupby("session_id")["user_id"].transform('min')})
+    sessions = sessions.dropna(subset=["user_id"])
+    print(sessions.shape[0])
+    sessions = sessions.astype({'user_id': 'int32', 'product_id': 'int32', 'purchase_id': "Int64"}, errors='ignore')
+    sessions.to_json("data/sessions_clean.jsonl", orient='records', lines=True)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +31,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # clean_products()
+    clean_sessions()
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
